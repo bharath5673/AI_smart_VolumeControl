69b19fe2f6f416273d32714c734ec48e3243876f
bharath5673 <s.bharath.2000@gmail.com>
2021-04-04 14:32:59 +0530

    first commit

--- vid2gif.py
+++
@@ -1,7 +0,0 @@
-from moviepy.editor import *
-
-# clip = (VideoFileClip("output.mp4").subclip((3.65),(4.0))
-#         .resize(0.3))
-clip = (VideoFileClip("output.mp4").subclip((6.5),(9.0)))
-# clip = (VideoFileClip("output.mp4"))       
-clip.write_gif("output2.gif")
